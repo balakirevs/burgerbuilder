09f9c3392e80c31c2dbb777a8b0b861d6c87afdb
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-20 20:52:16 +0100

    Adds order button

--- app/javascript/burger/components/Burger/BuildControls/BuildControls.js
+++ app/javascript/burger/components/Burger/BuildControls/BuildControls.js
@@ -21,6 +21,10 @@ const buildControls = (props) => (
         disabled={props.disabled[control.type]}
       />
     ))}
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasable}>ORDER NOW
+    </button>
   </div>
 );
 
