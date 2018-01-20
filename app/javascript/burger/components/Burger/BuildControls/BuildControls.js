a0ecdd95735565a9f005f3c492336af9d801f5a4
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-20 14:44:35 +0100

    Adds remove add disable button ingredients

---
+++ app/javascript/burger/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './BuildControl.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map(control => (
+      <BuildControl
+        key={control.label}
+        label={control.label}
+        added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)}
+        disabled={props.disabled[control.type]}
+      />
+    ))}
+  </div>
+);
+
+export default buildControls;
