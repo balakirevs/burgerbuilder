92539cabae536872bb671af8f12baba5bc05610b
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-21 21:43:57 +0100

    Collapse empty tags

--- app/javascript/burger/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ app/javascript/burger/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -8,27 +8,27 @@ class BurgerIngredient extends Component {
 
     switch (this.props.type) {
       case ('bread-bottom'):
-        ingredient = <div className={classes.BreadBottom}></div>;
+        ingredient = <div className={classes.BreadBottom}/>;
         break;
       case ('bread-top'):
         ingredient = (
           <div className={classes.BreadTop}>
-            <div className={classes.Seeds1}></div>
-            <div className={classes.Seeds2}></div>
+            <div className={classes.Seeds1} />
+            <div className={classes.Seeds2} />
           </div>
         );
         break;
       case ('meat'):
-        ingredient = <div className={classes.Meat}></div>
+        ingredient = <div className={classes.Meat} />;
         break;
       case ('cheese'):
-        ingredient = <div className={classes.Cheese}></div>
+        ingredient = <div className={classes.Cheese} />;
         break;
       case ('bacon'):
-        ingredient = <div className={classes.Bacon}></div>
+        ingredient = <div className={classes.Bacon} />;
         break;
       case ('salad'):
-        ingredient = <div className={classes.Salad}></div>
+        ingredient = <div className={classes.Salad} />;
         break;
       default:
         ingredient = null;
--- app/javascript/burger/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ app/javascript/burger/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -3,9 +3,9 @@ import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => (
   <div className={classes.DrawerToggle} onClick={props.clicked}>
-    <div></div>
-    <div></div>
-    <div></div>
+    <div />
+    <div />
+    <div />
   </div>
 );
 
