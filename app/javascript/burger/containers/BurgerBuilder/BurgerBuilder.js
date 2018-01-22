1049590dc3f0039234cba180618213b59c0bafad
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-22 22:05:51 +0100

    Adds axios, query-string with order dummy data

--- app/javascript/burger/containers/BurgerBuilder/BurgerBuilder.js
+++ app/javascript/burger/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import axios from 'axios';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -79,6 +80,28 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     this.setState({loading: true});
+
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Aleksandr Balakiriev',
+        address: {
+          street: 'C.-F. Ramuz 29',
+          zip: '1009',
+          country: 'Switzerland'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    };
+    axios.post(`api/orders/`, order)
+      .then(response => {
+        this.setState({loading: false, purchasing: false})
+      })
+      .catch(error => {
+        this.setState({loading: false, purchasing: false});
+      });
   };
 
   render () {
