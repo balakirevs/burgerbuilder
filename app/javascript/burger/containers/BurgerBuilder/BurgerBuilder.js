505b1fe4c3adc1aa24fdf4a3979b76d4e578cfe8
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-24 20:18:01 +0100

    Order submission & passing data between pages

--- app/javascript/burger/containers/BurgerBuilder/BurgerBuilder.js
+++ app/javascript/burger/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,33 +85,11 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    /*this.setState({loading: true});
-
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Aleksandr Balakiriev',
-        address: {
-          street: 'C.-F. Ramuz 29',
-          zip: '1009',
-          country: 'Switzerland'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    };
-    axios.post('https://burger-builder-3ed20.firebaseio.com/orders.json', order)
-      .then(response => {
-        this.setState({loading: false, purchasing: false})
-      })
-      .catch(error => {
-        this.setState({loading: false, purchasing: false});
-      });*/
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
