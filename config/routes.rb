2539f3673467d419e0bde789a9b55312b979dceb
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-24 16:05:04 +0100

    adds rails route to redirect to react SPA

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :ingredients, only: :index
+      get '*path', to: 'pages#home', constraints: ->(request) do
+        !request.xhr? && request.format.html?
+      end
     end
   end
 end
