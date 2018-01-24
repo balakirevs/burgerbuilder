a136ec6c6c065a19ad4380fd749dfbe7754abb06
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-24 20:55:22 +0100

    Adds Order page, Nav link to Order, Order routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/checkout/contact-data', to: 'pages#home', format: false
   get '/checkout', to: 'pages#home', format: false
+  get '/orders', to: 'pages#home', format: false
 
   namespace :api, defaults: { format: :json } do
     namespace :v1, constraints: ApiConstraints.new(version: 1, default: true) do
