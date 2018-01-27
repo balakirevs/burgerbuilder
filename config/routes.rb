8e32633efde10a288fcc178da12e1c83a30a2f79
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-27 19:13:33 +0100

    Adding Logout link, forwarding unauth users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/checkout', to: 'pages#home', format: false
   get '/orders', to: 'pages#home', format: false
   get '/auth', to: 'pages#home', format: false
+  get '/logout', to: 'pages#home', format: false
 
   namespace :api, defaults: { format: :json } do
     namespace :v1, constraints: ApiConstraints.new(version: 1, default: true) do
