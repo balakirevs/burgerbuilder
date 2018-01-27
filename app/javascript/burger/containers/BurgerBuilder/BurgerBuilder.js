0b23806978f2acf05c61499ad6e8e4ff1c1d1974
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-27 20:10:31 +0100

    change order button text if authenticated & if buying burger redirect to auth page when unauthed

--- app/javascript/burger/containers/BurgerBuilder/BurgerBuilder.js
+++ app/javascript/burger/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,7 +31,11 @@ class BurgerBuilder extends Component {
   };
 
   purchaseHandler = () => {
-    this.setState({purchasing: true})
+    if (this.props.isAuthenticated) {
+      this.setState({purchasing: true});
+    } else {
+      this.props.history.push('/auth');
+    }
   };
 
   purchaseCancelHandler = () => {
@@ -64,6 +68,7 @@ class BurgerBuilder extends Component {
             disabled={disabledInfo}
             purchasable={this.updatePurchaseState(this.props.ingredients)}
             ordered={this.purchaseHandler}
+            isAuth={this.props.isAuthenticated}
             price={this.props.price}
           />
         </Aux>
@@ -91,7 +96,8 @@ const mapStateToProps = state => {
   return {
     ingredients: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    error: state.burgerBuilder.error
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token !== null
   }
 };
 
