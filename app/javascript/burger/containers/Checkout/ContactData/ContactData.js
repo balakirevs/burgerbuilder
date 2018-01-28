fdcc445f02208a4db3a42c528d92feb76d7bc363
Aleksandr Balakiriev <balakirevs@i.ua>
2018-01-28 21:23:37 +0100

    Replaces utility file & refactoring with updateObject

--- app/javascript/burger/containers/Checkout/ContactData/ContactData.js
+++ app/javascript/burger/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import axios from 'axios';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import { updateObject } from '../../../shared/utility';
 
 class ContactData extends Component {
   state = {
@@ -114,16 +115,14 @@ class ContactData extends Component {
   };
 
   inputChangedHandler = (event, inputId) => {
-    const updatedOrderForm = {
-      ...this.state.orderForm
-    };
-    const updatedFormElement = {
-      ...updatedOrderForm[inputId]
-    };
-    updatedFormElement.value = event.target.value;
-    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
-    updatedFormElement.touched = true;
-    updatedOrderForm[inputId] = updatedFormElement;
+    const updatedFormElement = updateObject(this.state.orderForm[inputId], {
+      value: event.target.value,
+      valid: this.checkValidity(event.target.value, this.state.orderForm[inputId].validation),
+      touched: true
+    });
+    const updatedOrderForm = updateObject(this.state.orderForm, {
+      [inputId]: updatedFormElement
+    });
 
     let formIsValid = true;
     for (let inputId in updatedOrderForm) {
